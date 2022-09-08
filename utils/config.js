29f12e341ea1bfc405e4a90b631bcfcff3976a6d
Taylor Y <tdy721@gmail.com>
2022-09-08 14:48:19 -0700

    envalid

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,17 @@
 // Load dotenv over Deno.env
 import { config } from "$std/dotenv/mod.ts";
-const env = Object.assign(Deno.env.toObject(), await config());
+import { cleanEnv, host, port, str, url } from "envalid";
+
+const RAW_ENV = Object.assign(Deno.env.toObject(), await config());
+
+const ENV = cleanEnv(RAW_ENV, {
+  API_URL: url(),
+  BASE_URL: url(),
+  DENO_ENV: str({ choices: ["development", "testing", "production"] }),
+  REDIS_PASS: str(),
+  REDIS_HOST: host(),
+  REDIS_PORT: port(),
+});
 
 export const {
   API_URL,
@@ -9,6 +20,6 @@ export const {
   REDIS_HOST,
   REDIS_PASS,
   REDIS_PORT,
-} = env;
+} = ENV;
 
-export default env;
+export default ENV;
