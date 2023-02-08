df504eb4e9936dc23f0bac2fa8ae8269d41f28f1
Taylor Y <tdy721@gmail.com>
2023-02-08 15:13:38 -0700

    fix

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,10 @@
 import { load } from "$std/dotenv/mod.ts";
 import { cleanEnv, host, port, str, url } from "envalid";
 
-const ENV = cleanEnv(await load(), {
+// Loads stuff for deno.dev
+const RAW_ENV = Object.assign(Deno.env.toObject(), await load());
+
+const ENV = cleanEnv(RAW_ENV, {
   API_URL: url(),
   BASE_URL: url(),
   DENO_ENV: str({ choices: ["development", "testing", "production"] }),
