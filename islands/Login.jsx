eb66366a36ea0cbb96546295ed2c58c00180ba94
Taylor Y <tdy721@gmail.com>
2023-02-25 14:48:19 -0700

    better load formvalidation libs from inside the island not really sure if this is a good idea

--- islands/Login.jsx
+++ islands/Login.jsx
@@ -1,4 +1,13 @@
 import { useEffect, useRef, useState } from "preact/hooks"
+import { asset, Head } from "$fresh/runtime.ts"
+
+const LoadFormValidation = () => (
+  <Head>
+    <script
+      src={asset("./vendor/formvalidation/js/FormValidation.js")}
+    />
+  </Head>
+)
 
 const LoginForm = (props) => {
   const [buttonDisabled, setButtonDisabled] = useState(false)
@@ -46,70 +55,73 @@ const LoginForm = (props) => {
     // console.log(fv, form)
   }, [])
   return (
-    <form ref={form} class="space-y-6 mt-8" action="/login" method="POST">
-      <div class="validation-message text-red-500"></div>
-      <div class="rounded-md shadow-sm -space-y-px">
-        <div>
-          <label for="identifier" class="sr-only">Username</label>
-          <input
-            id="identifier"
-            name="identifier"
-            type="text"
-            required
-            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-yellow-300 focus:border-yellow-300 focus:z-10 sm:text-sm"
-            placeholder="Username"
-          />
+    <>
+      <LoadFormValidation />
+      <form ref={form} class="space-y-6 mt-8" action="/login" method="POST">
+        <div class="validation-message text-red-500"></div>
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="identifier" class="sr-only">Username</label>
+            <input
+              id="identifier"
+              name="identifier"
+              type="text"
+              required
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-yellow-300 focus:border-yellow-300 focus:z-10 sm:text-sm"
+              placeholder="Username"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-yellow-300 focus:border-yellow-300 focus:z-10 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
         </div>
-        <div>
-          <label for="password" class="sr-only">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autocomplete="current-password"
-            required
-            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-yellow-300 focus:border-yellow-300 focus:z-10 sm:text-sm"
-            placeholder="Password"
-          />
-        </div>
-      </div>
 
-      <div class="flex items-center justify-between">
-        <div class="text-sm">
-          <a
-            href="/forgot-password"
-            class="font-medium text-yellow-400 hover:text-yellow-300"
-          >
-            Forgot your password?
-          </a>
-        </div>
-      </div>
-      <div>
-        <button
-          type="submit"
-          id="login-submit"
-          class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-300"
-          disabled={buttonDisabled}
-        >
-          <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-            <svg
-              class="h-5 w-5 text-yellow-300 group-hover:text-yellow-400"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
+        <div class="flex items-center justify-between">
+          <div class="text-sm">
+            <a
+              href="/forgot-password"
+              class="font-medium text-yellow-400 hover:text-yellow-300"
             >
-              <path
-                fill-rule="evenodd"
-                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </span>
-          Sign in
-        </button>
-      </div>
-    </form>
+              Forgot your password?
+            </a>
+          </div>
+        </div>
+        <div>
+          <button
+            type="submit"
+            id="login-submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-300"
+            disabled={buttonDisabled}
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg
+                class="h-5 w-5 text-yellow-300 group-hover:text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+            Sign in
+          </button>
+        </div>
+      </form>
+    </>
   )
 }
 
--- routes/login/index.jsx
+++ routes/login/index.jsx
@@ -39,14 +39,6 @@ const PageLogin = ({ data }) => {
   const { error } = data
   return (
     <Layout data={data}>
-      <Head>
-        <script
-          src={asset("./vendor/formvalidation/js/FormValidation.js")}
-        />
-        <script
-          src={asset("./vendor/formvalidation/js/plugins/Pure.js")}
-        />
-      </Head>
       <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full">
           <div>
