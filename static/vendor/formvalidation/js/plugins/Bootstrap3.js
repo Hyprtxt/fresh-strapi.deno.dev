de2c650f2ee68e327792b0c1040f2849357f859d
Taylor Y <tdy721@gmail.com>
2023-02-25 19:07:53 -0700

    fmt

--- static/vendor/formvalidation/js/plugins/Bootstrap3.js
+++ static/vendor/formvalidation/js/plugins/Bootstrap3.js
@@ -3,175 +3,192 @@
  * The best validation library for JavaScript
  * (c) 2013 - 2021 Nguyen Huu Phuoc <me@phuoc.ng>
  */
-
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, (global.FormValidation = global.FormValidation || {}, global.FormValidation.plugins = global.FormValidation.plugins || {}, global.FormValidation.plugins.Bootstrap3 = factory()));
-})(this, (function () { 'use strict';
+;(function (global, factory) {
+  typeof exports === "object" && typeof module !== "undefined"
+    ? module.exports = factory()
+    : typeof define === "function" && define.amd
+    ? define(factory)
+    : (global = typeof globalThis !== "undefined" ? globalThis : global || self,
+      (global.FormValidation = global.FormValidation || {},
+        global.FormValidation.plugins = global.FormValidation.plugins || {},
+        global.FormValidation.plugins.Bootstrap3 = factory()))
+})(this, function () {
+  "use strict"
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
+      throw new TypeError("Cannot call a class as a function")
     }
   }
 
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
+      var descriptor = props[i]
+      descriptor.enumerable = descriptor.enumerable || false
+      descriptor.configurable = true
+      if ("value" in descriptor) descriptor.writable = true
+      Object.defineProperty(target, descriptor.key, descriptor)
     }
   }
 
   function _createClass(Constructor, protoProps, staticProps) {
-    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) _defineProperties(Constructor, staticProps);
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps)
+    if (staticProps) _defineProperties(Constructor, staticProps)
     Object.defineProperty(Constructor, "prototype", {
-      writable: false
-    });
-    return Constructor;
+      writable: false,
+    })
+    return Constructor
   }
 
   function _inherits(subClass, superClass) {
     if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function");
+      throw new TypeError("Super expression must either be null or a function")
     }
 
     subClass.prototype = Object.create(superClass && superClass.prototype, {
       constructor: {
         value: subClass,
         writable: true,
-        configurable: true
-      }
-    });
+        configurable: true,
+      },
+    })
     Object.defineProperty(subClass, "prototype", {
-      writable: false
-    });
-    if (superClass) _setPrototypeOf(subClass, superClass);
+      writable: false,
+    })
+    if (superClass) _setPrototypeOf(subClass, superClass)
   }
 
   function _getPrototypeOf(o) {
-    _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) {
-      return o.__proto__ || Object.getPrototypeOf(o);
-    };
-    return _getPrototypeOf(o);
+    _getPrototypeOf = Object.setPrototypeOf
+      ? Object.getPrototypeOf.bind()
+      : function _getPrototypeOf(o) {
+        return o.__proto__ || Object.getPrototypeOf(o)
+      }
+    return _getPrototypeOf(o)
   }
 
   function _setPrototypeOf(o, p) {
-    _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) {
-      o.__proto__ = p;
-      return o;
-    };
-    return _setPrototypeOf(o, p);
+    _setPrototypeOf = Object.setPrototypeOf
+      ? Object.setPrototypeOf.bind()
+      : function _setPrototypeOf(o, p) {
+        o.__proto__ = p
+        return o
+      }
+    return _setPrototypeOf(o, p)
   }
 
   function _isNativeReflectConstruct() {
-    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-    if (Reflect.construct.sham) return false;
-    if (typeof Proxy === "function") return true;
+    if (typeof Reflect === "undefined" || !Reflect.construct) return false
+    if (Reflect.construct.sham) return false
+    if (typeof Proxy === "function") return true
 
     try {
-      Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {}));
-      return true;
+      Boolean.prototype.valueOf.call(
+        Reflect.construct(Boolean, [], function () {}),
+      )
+      return true
     } catch (e) {
-      return false;
+      return false
     }
   }
 
   function _assertThisInitialized(self) {
     if (self === void 0) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+      throw new ReferenceError(
+        "this hasn't been initialised - super() hasn't been called",
+      )
     }
 
-    return self;
+    return self
   }
 
   function _possibleConstructorReturn(self, call) {
     if (call && (typeof call === "object" || typeof call === "function")) {
-      return call;
+      return call
     } else if (call !== void 0) {
-      throw new TypeError("Derived constructors may only return object or undefined");
+      throw new TypeError(
+        "Derived constructors may only return object or undefined",
+      )
     }
 
-    return _assertThisInitialized(self);
+    return _assertThisInitialized(self)
   }
 
   function _createSuper(Derived) {
-    var hasNativeReflectConstruct = _isNativeReflectConstruct();
+    var hasNativeReflectConstruct = _isNativeReflectConstruct()
 
     return function _createSuperInternal() {
       var Super = _getPrototypeOf(Derived),
-          result;
+        result
 
       if (hasNativeReflectConstruct) {
-        var NewTarget = _getPrototypeOf(this).constructor;
+        var NewTarget = _getPrototypeOf(this).constructor
 
-        result = Reflect.construct(Super, arguments, NewTarget);
+        result = Reflect.construct(Super, arguments, NewTarget)
       } else {
-        result = Super.apply(this, arguments);
+        result = Super.apply(this, arguments)
       }
 
-      return _possibleConstructorReturn(this, result);
-    };
+      return _possibleConstructorReturn(this, result)
+    }
   }
 
-  var e = FormValidation.utils.classSet;
+  var e = FormValidation.utils.classSet
 
-  var t = FormValidation.utils.hasClass;
+  var t = FormValidation.utils.hasClass
 
-  var s = FormValidation.plugins.Framework;
+  var s = FormValidation.plugins.Framework
 
-  var n = /*#__PURE__*/function (_s) {
-    _inherits(n, _s);
+  var n = /*#__PURE__*/ function (_s) {
+    _inherits(n, _s)
 
-    var _super = _createSuper(n);
+    var _super = _createSuper(n)
 
     function n(e) {
-      _classCallCheck(this, n);
+      _classCallCheck(this, n)
 
-      return _super.call(this, Object.assign({}, {
-        formClass: "fv-plugins-bootstrap3",
-        messageClass: "help-block",
-        rowClasses: "has-feedback",
-        rowInvalidClass: "has-error",
-        rowPattern: /^(.*)(col|offset)-(xs|sm|md|lg)-[0-9]+(.*)$/,
-        rowSelector: ".form-group",
-        rowValidClass: "has-success"
-      }, e));
+      return _super.call(
+        this,
+        Object.assign({}, {
+          formClass: "fv-plugins-bootstrap3",
+          messageClass: "help-block",
+          rowClasses: "has-feedback",
+          rowInvalidClass: "has-error",
+          rowPattern: /^(.*)(col|offset)-(xs|sm|md|lg)-[0-9]+(.*)$/,
+          rowSelector: ".form-group",
+          rowValidClass: "has-success",
+        }, e),
+      )
     }
 
     _createClass(n, [{
       key: "onIconPlaced",
       value: function onIconPlaced(s) {
         e(s.iconElement, {
-          "form-control-feedback": true
-        });
-        var _n = s.element.parentElement;
+          "form-control-feedback": true,
+        })
+        var _n = s.element.parentElement
 
         if (t(_n, "input-group")) {
-          _n.parentElement.insertBefore(s.iconElement, _n.nextSibling);
+          _n.parentElement.insertBefore(s.iconElement, _n.nextSibling)
         }
 
-        var r = s.element.getAttribute("type");
+        var r = s.element.getAttribute("type")
 
         if ("checkbox" === r || "radio" === r) {
-          var _e = _n.parentElement;
+          var _e = _n.parentElement
 
           if (t(_n, r)) {
-            _n.parentElement.insertBefore(s.iconElement, _n.nextSibling);
+            _n.parentElement.insertBefore(s.iconElement, _n.nextSibling)
           } else if (t(_n.parentElement, r)) {
-            _e.parentElement.insertBefore(s.iconElement, _e.nextSibling);
+            _e.parentElement.insertBefore(s.iconElement, _e.nextSibling)
           }
         }
-      }
-    }]);
+      },
+    }])
 
-    return n;
-  }(s);
+    return n
+  }(s)
 
-  return n;
-
-}));
+  return n
+})
